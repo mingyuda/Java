0e089fafae19a29acee0ea50c5c4e1d32667d956
dbflwkdj <dbflwkdj7@naver.com>
2023-04-01 16:35:06 +0900

    practice IO

---
+++ step02_practice/src/step09IO/Test.java
@@ -0,0 +1,15 @@
+package step09IO;
+
+import java.util.Scanner;
+
+public class Test {
+
+
+	public static void main(String[] args) {
+		Scanner inData = new Scanner(System.in);
+		String i = inData.next();
+		System.out.println(i);
+
+	}
+
+}

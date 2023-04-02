34522cf347e5e0e1d42877c478537d2f0e5b6d91
dbflwkdj <dbflwkdj7@naver.com>
2023-04-03 00:00:12 +0900

    practice 0402 add anotation

--- step02_practice/src/step11Network/Client.java
+++ step02_practice/src/step11Network/Client.java
@@ -13,6 +13,10 @@ public class Client {
 			socket.connect(new InetSocketAddress("localhost", 9999));
 			System.out.println("연결 성공.");
 			
+			// OutputStream으로 메세지를 보내고 InputStream으로 메세지를 받을 수 있다.
+			// OutputStream - getOutputStream, InputStream - getInputStream 연결
+			// 보낼때 write, flush
+			// 받을 때는 객체 생성후 read(변수명)
 			
 			
 		} catch (IOException e) {
